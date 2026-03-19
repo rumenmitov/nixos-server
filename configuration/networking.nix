4c2a212a1b62935e8f663f6b956103bd07df346c
Rumen <rumenmitov@protonmail.com>
2026-03-19 09:42:55 +0100

    nginx: increased client body size to 100M

--- configuration/networking.nix
+++ configuration/networking.nix
@@ -46,6 +46,9 @@
           locations."/" = {
             proxyPass = "http://127.0.0.1:11000";
             proxyWebsockets = true;
+            extraConfig = ''
+            client_max_body_size 100M;
+            '';
           };
         };
 
